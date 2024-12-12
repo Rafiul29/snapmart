435f6adb1fe501a7d1ee005fef30d0235a50e1c5
Rafiul29 <rafiulislam4472@gmail.com>
2024-12-13 00:10:40 +0600

    category, product, stock serializer added

---
+++ store/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Category, Product, Stock
+
+class CategorySerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model=Category
+    fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+  category=CategorySerializer()
+
+  class Meta:
+    model=Product
+    fields='__all__'
+
+
+class StockSerializer(serializers.ModeSerializer):
+  product=ProductSerializer()
+
+  class Meta:
+    model =Stock
+    fields='__all__'
